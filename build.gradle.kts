1838ec5cfe47a09d11a8ac415618a8c313e538b0
Marcelo A Tabone <marceloatabone@hotmail.com>
2021-08-24 17:40:41 -0300

    Put Compose in code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import org.jetbrains.compose.compose
+
+
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.5.10"
+    //id("org.jetbrains.kotlin.js") version "1.5.10"
+    kotlin("multiplatform") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha1-rc5"
 }
 
 group = "org.example"
@@ -7,12 +12,37 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-js"))
+   // implementation(kotlin("stdlib-js"))
 }
 
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val jsMain by getting {
+            kotlin.srcDir("src/main/kotlin")
+            resources.srcDir("src/main/resources")
+
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.webpackDevServer.version =
+        "4.0.0-rc.0"
+}
+/*
 kotlin {
     js {
         browser {
@@ -34,8 +64,8 @@ kotlin {
         binaries.executable()
     }
 }
-
 rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.webpackDevServer.version =
         "4.0.0-rc.0"
 }
+*/
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,47 @@
-import kotlinx.browser.document
+import androidx.compose.runtime.*
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Style
+import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.renderComposable
+
 
 fun main() {
-    document.write("Hello, Kotlin WEB =]")
+    renderComposable(rootElementId = "head") {
+        Head()
+    }
+    renderComposable(rootElementId = "root") {
+        Body()
+    }
+    renderComposable(rootElementId = "foot") {
+        Foot()
+    }
 }
+
+@Composable
+fun Head() {
+
+    Div {
+
+    }
+}
+
+@Composable
+fun Body() {
+
+    var greeting by remember { mutableStateOf(greet()) }
+    Button(attrs = { onClick { greeting = greet() } }) {
+        Text(greeting)
+    }
+
+}
+
+@Composable
+fun Foot() {
+
+
+
+}
+
+fun greet() = listOf("Hello", "Hallo", "Hola", "Servus").random()
+
